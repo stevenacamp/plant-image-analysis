5f00d81048acdefa4c4091f74cd2ec1be1e860c4
Trent Spice <tspice23@att.net>
2019-04-24 20:16:48 -0400

    using firebase file uploader

--- src/routes/identify/upload.js
+++ src/routes/identify/upload.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, CardContent, Grid, IconButton, withStyles } from '@material-ui/core';
 import { AddAPhotoRounded } from '@material-ui/icons';
-import Dropzone from 'react-dropzone';
+import FileUploader from 'react-firebase-file-uploader';
 import firebase from "firebase";
 
 const styles = {
@@ -15,17 +15,28 @@ class Upload extends React.Component {
         url: ''
     }
 
-    onDrop = files => {
-        const file = files[0];
-        const name = (+new Date()) + '-' + file.name;
-        const metadata = { contentType: file.type };
-        firebase
-            .storage()
-            .ref("images")
-            .child(name)
-            .put(file, metadata)
-            .then(snapshot => snapshot.ref.getDownloadURL())
-            .then(url => this.setState({ url }))
+    async handleUploadSuccess (filename) {
+        try {
+            let { bucket, fullPath } = await firebase.storage().ref('images').child(filename).getMetadata();
+            console.log('bucket', bucket);
+            console.log('fullPath', fullPath);
+            let downloadUrl = await firebase.storage().ref('images').child(filename).getDownloadURL();
+            console.log('downloadURL', downloadUrl);
+
+            let newImage = {
+                url: downloadUrl,
+                bucket,
+                fullPath
+            }
+
+            console.log('newImage', newImage);
+
+            await firebase.firestore().collection('images').add(newImage);
+        }
+
+        catch(err) {
+            console.error(err);
+        }
     }
 
     render() {
@@ -34,26 +45,23 @@ class Upload extends React.Component {
             <CardContent>
                 <Grid container justify="center" alignContent="center">
                     {this.state.url && <img src={this.state.url} alt="plant" />}
-                    <Dropzone onDrop={this.onDrop}>
-                        {({ getRootProps, getInputProps }) => (
-                            <div
-                                {...getRootProps({
-                                    style: {
-                                        width: 'fit-content',
-                                        borderRadius: '50%'
-                                    }
-                                })}
-                            >
-                                <input {...getInputProps()} />
-                                <IconButton style={{ padding: 32, border: "2px solid #ffc400" }}>
-                                    <AddAPhotoRounded style={{ height: 150, width: 150 }} color="secondary" />
-                                </IconButton>
-                            </div>
-                        )}
-                        </Dropzone>
-                        <Grid item xs={12} container justify="flex-end" className={classes.submitContainer}>
-                            <Button onClick={this.props.submitImage} variant="contained" color="primary">Next</Button>
-                        </Grid>
+                    
+                    <label>
+                    <AddAPhotoRounded style={{ height: 150, width: 150 }} color="secondary" />
+                    <FileUploader
+                                hidden
+                                accept="image/*"
+                                storageRef={firebase.storage().ref('images')}
+                                onUploadStart={this.handleUploadStart}
+                                onUploadError={this.handleUploadError}
+                                onUploadSuccess={this.handleUploadSuccess}
+                                onProgress={this.handleProgress}
+                        />
+                    </label>
+
+                    <Grid item xs={12} container justify="flex-end" className={classes.submitContainer}>
+                        <Button onClick={this.props.submitImage} variant="contained" color="primary">Next</Button>
+                    </Grid>
                 </Grid>
             </CardContent>
         );
